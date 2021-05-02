df433c0d878dd9b5bb28fcae76ecba4d251c8114
Maratib <maratib@gmail.com>
2021-05-02 05:30:08 +0500

    vite aliases added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { getAliases } from "vite-aliases";
+import vue from "@vitejs/plugin-vue";
 
+const aliases = getAliases();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: aliases,
+  },
+  plugins: [vue()],
+});
